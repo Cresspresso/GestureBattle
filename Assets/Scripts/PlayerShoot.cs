7c1f7ed26f38b8afa6e87cc90f10cb2091bb6eaf
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-03-05 13:02:01 +1300

    PewGunBullet

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for shooting projectiles from a player character.
+/// </summary>
+/// <author>Elijah Shadbolt</author>
+public class PlayerShoot : MonoBehaviour
+{
+	public PewGunBullet bulletPrefab;
+	public Transform spawnLocation;
+	public float muzzleVelocity = 100.0f;
+
+	public void Discharge()
+	{
+		var bullet = Instantiate(bulletPrefab, spawnLocation.position, spawnLocation.rotation);
+		bullet.owner = this;
+		bullet.GetComponent<Rigidbody>().velocity = spawnLocation.forward * muzzleVelocity;
+	}
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Discharge();
+		}
+	}
+}
