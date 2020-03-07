26176b63c240758bbd32056d14fea349739aa40b
Jordyn <53497238+Ooogway@users.noreply.github.com>
2020-03-07 17:39:57 +1300

    Umm... did a wee bit

---
+++ Assets/Scripts/HologramFacePlayer1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramFacePlayer1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    void OnTriggerStay(Collider other)
+    {
+        transform.LookAt(target);
+    }
+
+}
