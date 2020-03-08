964ad0098b77a3e49303872abdb17e63631698d1
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-03-09 09:47:48 +1300

    rotate hologram script

--- Assets/Scripts/HologramFacePlayer.cs
+++ Assets/Scripts/HologramFacePlayer.cs
@@ -2,14 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Rotates to face the player if they are close, otherwise returns to normal.
+/// </summary>
+/// <author>Elijah Shadbolt</author>
 public class HologramFacePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Transform target;
+	public PlayerEntity player;
+	private Transform target;
 
-    void OnTriggerStay(Collider other)
-    {
-        transform.LookAt(target);
-    }
+	public float lerpTime = 0.3f;
+	private float elapsedTime = 0.0f;
 
+	void OnTriggerEnter(Collider other)
+	{
+		var op = other.GetComponentInParent<PlayerEntity>();
+		if (op)
+		{
+			this.player = op;
+			target = op.moveCamera.player;
+		}
+	}
+
+	private void LateUpdate()
+	{
+		if (player)
+		{
+			elapsedTime += Time.deltaTime;
+		}
+		else
+		{
+			elapsedTime -= Time.deltaTime;
+		}
+		elapsedTime = Mathf.Clamp(elapsedTime, 0.0f, lerpTime);
+		if (target)
+		{
+			transform.rotation = Quaternion.Lerp(
+				transform.parent ? transform.parent.rotation : Quaternion.identity,
+				Quaternion.LookRotation(target.position - transform.position),
+				elapsedTime / lerpTime);
+		}
+		else
+		{
+			transform.localRotation = Quaternion.identity;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		var op = other.GetComponentInParent<PlayerEntity>();
+		if (op == player)
+		{
+			this.player = null;
+			// do not set target to null, because rotating back
+		}
+	}
 }
---
+++ Assets/Scripts/PlayerEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Placed on the root of a player character, represents a unique player object.
+/// </summary>
+/// <author>Elijah Shadbolt</author>
+public class PlayerEntity : MonoBehaviour
+{
+	private MoveCamera m_moveCamera;
+	public MoveCamera moveCamera { get { if (!m_moveCamera) { m_moveCamera = GetComponentInChildren<MoveCamera>(); } return m_moveCamera; } }
+
+	private PlayerShoot m_shoot;
+	public PlayerShoot shoot { get { if (!m_shoot) { m_shoot = GetComponentInChildren<PlayerShoot>(); } return m_shoot; } }
+
+	private PlayerReload m_reload;
+	public PlayerReload reload { get { if (!m_reload) { m_reload = GetComponentInChildren<PlayerReload>(); } return m_reload; } }
+
+	private PlayerMovement m_movement;
+	public PlayerMovement movement { get { if (!m_movement) { m_movement = GetComponentInChildren<PlayerMovement>(); } return m_movement; } }
+}
