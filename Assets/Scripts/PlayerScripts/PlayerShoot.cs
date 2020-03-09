809ef4254af82666f1d1841ff07a619cc92f55c6
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-03-09 15:12:41 +1300

    animations work whaterver

---
+++ Assets/Scripts/PlayerScripts/PlayerShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for shooting projectiles from a player character.
+/// </summary>
+/// <author>Elijah Shadbolt</author>
+public class PlayerShoot : MonoBehaviour
+{
+	private PlayerEntity m_player;
+	public PlayerEntity player { get { if (!m_player) { m_player = GetComponentInParent<PlayerEntity>(); } return m_player; } }
+
+	public ProjectileBullet bulletPrefab;
+	public Transform spawnLocation;
+
+	public ProjectileBullet Discharge()
+	{
+		/// <author>Elijah Shadbolt</author>
+		var bullet = Instantiate(bulletPrefab, spawnLocation.position, spawnLocation.rotation);
+		bullet.OnSpawned(this);
+
+		return bullet;
+	}
+}
