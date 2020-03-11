5be179f9f932c059a57ac339826ea519f24aadb7
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-03-12 08:52:15 +1300

    main menu buttons

---
+++ Assets/MenuPlayButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuPlayButton : MonoBehaviour
+{
+	private void Awake()
+	{
+		var b = GetComponent<Button>();
+		b.onClick.RemoveListener(OnClick);
+		b.onClick.AddListener(OnClick);
+	}
+
+	void OnClick()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/MenuQuitButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuQuitButton : MonoBehaviour
+{
+	private void Awake()
+	{
+		var b = GetComponent<Button>();
+		b.onClick.RemoveListener(OnClick);
+		b.onClick.AddListener(OnClick);
+	}
+
+	void OnClick()
+	{
+		Quitter.Quit();
+	}
+}
