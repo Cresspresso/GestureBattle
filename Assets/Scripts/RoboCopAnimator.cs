7b1dc1fd6a5b68549999ca5c3eb452e0e701b72e
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-03-12 11:37:27 +1300

    enemy robocop animation scripting

---
+++ Assets/Scripts/RoboCopAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <author>Elijah Shadbolt</author>
+public class RoboCopAnimator : MonoBehaviour
+{
+	[SerializeField]
+	private Animator m_anim;
+	public Animator anim { get { if (!m_anim) { m_anim = GetComponent<Animator>(); } return m_anim; } }
+
+	public void TriggerKill()
+	{
+		if (anim) { anim.SetTrigger("Kill"); }
+	}
+
+	public int SpeedCategory {
+		get => anim.GetInteger("SpeedCategory");
+		set => anim.SetInteger("SpeedCategory", value);
+	}
+
+	/// <summary>Custom Event</summary>
+	/// <author>Elijah Shadbolt</author>
+	void OnKilled()
+	{
+		TriggerKill();
+	}
+}
